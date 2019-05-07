8f81ed24407c8a13d35dbeae3f98e06fe317037f
lazedo <luis.azedo@factorlusitano.com>
2019-05-07 02:05:52 +0000

    use kz_att_xfer and start move to cmd folder

--- applications/ecallmgr/src/calll_cmd/ecallmgr_fs_transfer.erl
+++ applications/ecallmgr/src/calll_cmd/ecallmgr_fs_transfer.erl
@@ -18,34 +18,30 @@
                           when Node :: atom().
 attended(Node, UUID, JObj) ->
     TransferTo = kz_json:get_ne_binary_value(<<"Transfer-To">>, JObj),
-    CCVs = kz_json:get_json_value(<<"Custom-Channel-Vars">>, JObj, kz_json:new()),
-
-    Realm = transfer_realm(UUID, CCVs),
-
+    Realm = transfer_realm(UUID, JObj),
     ReqURI = <<TransferTo/binary, "@", Realm/binary>>,
 
-    Vars = [{<<"Ignore-Early-Media">>, <<"ring_ready">>}
+    Vars = [{<<"SIP-Invite-Domain">>, Realm}
            ,{<<"Simplify-Loopback">>, <<"false">>}
            ,{<<"Loopback-Bowout">>, <<"true">>}
            ,{<<"Loopback-Request-URI">>, ReqURI}
-           ,{<<"SIP-Invite-Domain">>, Realm}
+           ,{<<"Ignore-Early-Media">>, <<"ring_ready">>}
            ,{<<"Outbound-Caller-ID-Number">>, kz_json:get_ne_binary_value(<<"Caller-ID-Number">>, JObj)}
            ,{<<"Outbound-Caller-ID-Name">>, kz_json:get_ne_binary_value(<<"Caller-ID-Name">>, JObj)}
            ,{<<"Outbound-Callee-ID-Number">>, TransferTo}
            ,{<<"Outbound-Callee-ID-Name">>, TransferTo}
-           ,{<<"Timeout">>, 60}
+           ,{<<"Timeout">>, kz_json:get_integer_value(<<"Timeout">>, JObj)}
            ],
-
-    Props = add_transfer_ccvs_to_vars(CCVs, Vars),
-    Exports = ecallmgr_util:process_fs_kv(Node, UUID, Props, 'set'),
-    Arg = lists:join(",", lists:flatten(Exports)),
+    Props = transfer_vars(JObj, Vars),
+    Arg = kz_binary:join(ecallmgr_util:process_fs_kv(Node, UUID, Props, 'set'), <<",">>),
 
     TransferContext = transfer_context(JObj),
+    To = <<TransferTo/binary, "@", Realm/binary>>,
+    KeyVars = transfer_keys(Node, UUID, JObj),
+    lager:info("transferring to (~s) ~s @ ~s on context ~s", [To, TransferTo, Realm, TransferContext]),
 
-    lager:info("transferring to ~s @ ~s on context ~s", [TransferTo, Realm, TransferContext]),
-
-    {<<"att_xfer">>
-    ,list_to_binary(["{", Arg, "}loopback/", TransferTo, <<"/">>, TransferContext])
+    {<<"kz_att_xfer">>
+    ,list_to_binary([KeyVars, "{", Arg, "}loopback/", TransferTo, <<"/">>, TransferContext])
     ,Node
     ,[{"hold-bleg", "true"}]
     }.
@@ -55,23 +51,40 @@ attended(Node, UUID, JObj) ->
 blind(Node, UUID, JObj) ->
     TransferTo = kz_json:get_ne_binary_value(<<"Transfer-To">>, JObj),
 
-    Realm = transfer_realm(UUID),
+    Realm = transfer_realm(UUID, JObj),
     TransferLeg = transfer_leg(JObj),
     TargetUUID = transfer_set_callid(UUID, TransferLeg),
 
-    lager:info("transferring to ~s @ ~s", [TransferTo, Realm]),
+    lager:info("transferring ~s to ~s @ ~s", [TargetUUID, TransferTo, Realm]),
 
     KVs = [{<<"SIP-Refer-To">>, <<"<sip:", TransferTo/binary, "@", Realm/binary, ">">>}
           ,{<<"SIP-Referred-By">>, transfer_referred(UUID, TransferLeg)}
+          ,{<<"Signal-Bridge-To">>, UUID}
           ],
-    Args = kz_binary:join(ecallmgr_util:process_fs_kv(Node, TargetUUID, KVs, 'set'), <<";">>),
-    [{<<"kz_uuid_setvar_multi">>, list_to_binary([TargetUUID, " ", Args])}
+    AppArgs = ecallmgr_util:multi_set_args(Node, TargetUUID, props:filter_undefined(KVs)),
+    [{<<"kz_uuid_multiset_encoded">>, list_to_binary([TargetUUID, " ", AppArgs])}
     ,{<<"blind_xfer">>, list_to_binary([TransferLeg, " ", TransferTo, <<" XML ">>, transfer_context(JObj)])}
     ].
 
--spec add_transfer_ccvs_to_vars(kz_json:object(), kz_term:proplist()) -> kz_term:proplist().
-add_transfer_ccvs_to_vars(CCVs, Vars) ->
-    kz_json:foldl(fun add_transfer_ccv_to_vars/3, Vars, CCVs).
+-spec transfer_keys(atom(), kz_term:ne_binary(), kz_json:object()) -> binary().
+transfer_keys(Node, UUID, JObj) ->
+    TransferKeys = kz_json:get_json_value(<<"Attended-Transfer-Keys">>, JObj, kz_json:new()),
+    TransferVars = kz_json:foldl(fun add_transfer_key/3, [], TransferKeys),
+    case ecallmgr_util:process_fs_kv(Node, UUID, TransferVars, 'set') of
+        [] -> <<>>;
+        Exports -> list_to_binary(["%^[", kz_binary:join(Exports, <<"^">>), "]"])
+    end.
+
+add_transfer_key(K, V, Vars) ->
+    [{<<"Attended-Transfer-", K/binary, "-Key">>, V} | Vars].
+
+-spec transfer_vars(kz_json:object(), kz_term:proplist()) -> kz_term:proplist().
+transfer_vars(JObj, Vars) ->
+    kz_json:foldl(fun add_transfer_ccv_to_vars/3, props:filter_undefined(Vars), transfer_ccvs(JObj)).
+
+-spec transfer_ccvs(kz_json:object()) -> kz_json:object().
+transfer_ccvs(JObj) ->
+    kz_json:get_json_value(<<"Custom-Channel-Vars">>, JObj, kz_json:new()).
 
 -spec add_transfer_ccv_to_vars(kz_json:key(), kz_json:json_term(), kz_term:proplist()) ->
                                       kz_term:proplist().
@@ -92,8 +105,8 @@ transfer_realm(UUID) ->
     end.
 
 -spec transfer_realm(kz_term:ne_binary(), kz_json:object()) -> kz_term:ne_binary().
-transfer_realm(UUID, CCVs) ->
-    case kz_json:get_first_defined([<<"Account-Realm">>, <<"Realm">>], CCVs) of
+transfer_realm(UUID, JObj) ->
+    case kz_json:get_first_defined([<<"Account-Realm">>, <<"Realm">>], transfer_ccvs(JObj)) of
         'undefined' -> transfer_realm(UUID);
         Realm -> Realm
     end.
@@ -110,7 +123,9 @@ transfer_set_callid(UUID, _) -> UUID.
 -spec transfer_referred(kz_term:ne_binary(), binary()) -> kz_term:api_binary().
 transfer_referred(UUID, <<"-bleg">>) ->
     case ecallmgr_fs_channel:fetch(UUID, 'record') of
-        {'ok', #channel{username='undefined'}} -> 'undefined';
+        {'ok', #channel{username='undefined'}=C} ->
+            lager:debug_unsafe("USER UNDEFINED : ~p", [C]),
+            'undefined';
         {'ok', #channel{username=Username
                        ,realm=Realm
                        }} -> <<"<sip:", Username/binary, "@", Realm/binary, ">">>;
@@ -118,7 +133,9 @@ transfer_referred(UUID, <<"-bleg">>) ->
     end;
 transfer_referred(UUID, _) ->
     case ecallmgr_fs_channel:fetch_other_leg(UUID, 'record') of
-        {'ok', #channel{username='undefined'}} -> 'undefined';
+        {'ok', #channel{username='undefined'}=C} ->
+            lager:debug_unsafe("USER UNDEFINED : ~p", [C]),
+            'undefined';
         {'ok', #channel{username=Username
                        ,realm=Realm
                        }} -> <<"<sip:", Username/binary, "@", Realm/binary, ">">>;
